efec05e393bc3d163de47e638bf3ccc6fbd17ebc
dud1337 <grant@dud.li>
2021-10-30 17:48:46 +0200

    favicons

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/media");
   eleventyConfig.addPassthroughCopy("./src/favicon");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   return {
     dir: {
       input: "src",
